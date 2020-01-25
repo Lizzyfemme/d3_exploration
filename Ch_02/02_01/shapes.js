4b8a82a535bc5c0f92b2f45e66e8a02a6e249c9c
Lizzyfemme <lizzy_swann@hotmail.com>
2020-01-24 18:19:43 -0800

    cirlces and ellipses added

--- Ch_02/02_01/shapes.js
+++ Ch_02/02_01/shapes.js
@@ -1,1 +1,15 @@
+const dataArray = [5, 8, 11];
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", "100%")
+  .attr("width", "100%");
 
+svg
+  .selectAll("circle")
+  .data(dataArray)
+  .enter()
+  .append("circle")
+  .attr("cx", "")
+  .attr("cy", "")
+  .attr("r", "");
--- bar_chart/shapes.js
+++ bar_chart/shapes.js
@@ -21,14 +21,35 @@ svg
   .attr("y", function(d, i) {
     return 300 - d * 15;
   });
-
+let xCircleAxis = 300;
 svg
-  .selectAll("circle")
+  .selectAll("circle.circle")
   .data(dataArray)
   .enter()
   .append("circle")
   .attr("cx", function(d, i) {
-    return 300 + i * 20;
+    xCircleAxis += d * 3 + i * 20;
+    return xCircleAxis;
   })
   .attr("cy", "100")
-  .attr("r", "r");
+  .attr("class", "circle")
+  .attr("r", function(d, i) {
+    return d * 3;
+  });
+
+let xEllipseAxis = 600;
+svg
+  .selectAll("ellipse.ellipse")
+  .data(dataArray)
+  .enter()
+  .append("ellipse")
+  .attr("cx", function(d, i) {
+    xEllipseAxis += d * 3 + i * 20;
+    return xEllipseAxis;
+  })
+  .attr("cy", "100")
+  .attr("class", "ellipse")
+  .attr("rx", function(d, i) {
+    return d * 3;
+  })
+  .attr("ry", "50");
